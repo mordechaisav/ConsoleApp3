344fb0ddee891593933e05cb4bea237a481a72fd
Dell-pc <Dell-pc@FHGFD>
2024-07-21 16:36:12 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true) {
+            Console.WriteLine("1. Create questions");
+            Console.WriteLine("2. Answer Questions");
+            string option = Console.ReadLine();
+            Server server = new Server("XML1.xml");
+
+                switch (option)
+                {
+                    case "1":
+                        server.CreateQuestion();
+                        break;
+                    case "2":
+                        server.read();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+
+        
+
+
+
+    }
+}
---
+++ ConsoleApp3/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp3
+{
+    internal class Server
+    {
+        List<string> questions = new List<string>();
+        List<string> answers = new List<string>();
+        string xmlFilePath;
+        public Server(string path) {
+            xmlFilePath = path;
+
+
+        }
+        public void CreateQuestion()
+
+
+
+        {
+            Console.WriteLine("Enter the question:");
+            string? question = Console.ReadLine();
+            Console.WriteLine("Enter the answer:");
+            string? answer = Console.ReadLine();
+
+            XElement xml1 =(File.Exists(xmlFilePath))?XElement.Load(xmlFilePath): new XElement("data");
+            xml1.Add(ToXml(question, answer));
+            xml1.Save(xmlFilePath);
+        }
+
+        public XElement ToXml(string question, string answer)
+        {
+            XElement element = new XElement("Item",
+                new XElement("Question", question),
+                new XElement("Answer", answer));
+
+            return element;
+        }
+
+        public void read()
+        {
+            if (!File.Exists(xmlFilePath))
+            {
+                Console.WriteLine("no qwetions");
+                return;
+            }
+            XElement xml = XElement.Load(xmlFilePath);
+            foreach (var item in xml.Elements("Item"))
+            {
+
+                var Question = item.Element("Question")?.Value;
+                var answer = item.Element("answer")?.Value;
+                questions.Add(Question);
+                answers.Add(answer);
+            }
+
+            foreach (var item in questions)
+            {
+                Console.WriteLine(item);
+            }
+            int choice = Console.Read();
+            Console.WriteLine(questions[choice-1]);
+            string ans = Console.ReadLine();
+            string result = ans == answers[choice - 1] ? "correct" : "incorrect"; 
+            Console.WriteLine(result);
+        }
+    }
+}
